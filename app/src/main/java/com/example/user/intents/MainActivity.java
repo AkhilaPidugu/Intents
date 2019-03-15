25f24427b92ea59e265178ea1f44a4c4265240c9
akhilapidugu99 <akhilapidugu99@gmail.com>
2019-03-15 19:03:39 +0530

    Intents

---
+++ app/src/main/java/com/example/user/intents/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.user.intents;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et;
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et = (EditText) findViewById(R.id.number);
+        iv = (ImageView) findViewById(R.id.dail);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri u = Uri.parse("tel:" + et.getText().toString());
+                Intent i = new Intent(Intent.ACTION_CALL, u);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(i);
+            }
+        });
+    }
+
+    public void gotoNext(View view) {
+        Intent i=new Intent(getApplicationContext(),SecondActivity.class);
+        startActivity(i);
+    }
+}
